88abbfa5be4b3a64d3515d5ea0420940be406352
inu-hayashi <takomisonikomi@gmail.com>
2020-03-23 18:00:55 +0900

    原材料名取得

--- vm/scraping/getInfoKirin.py
+++ vm/scraping/getInfoKirin.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.kirin.co.jp/products/list/nutrition/softdrink/'
 r = requests.get(url)
-
 soup = BeautifulSoup(r.content, 'lxml')
-titles = soup.select(".thumb")
-for title in titles:
-    print(title.text)
 
+# titles = soup.select(".thumb")
+# for title in titles:
+#     print(title.text)
+
+materials = soup.select('.explanation')
+for material in materials:
+    print(material.text)
