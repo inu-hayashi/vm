e1f98d5f1d2be3293ff286c8221aefab583594eb
inu-hayashi <takomisonikomi@gmail.com>
2020-03-23 17:54:18 +0900

    キリンタイトルスクレイピング

--- vm/scraping/getInfoKirin.py
+++ vm/scraping/getInfoKirin.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 url = 'https://www.kirin.co.jp/products/list/nutrition/softdrink/'
 r = requests.get(url)
 
-soup = BeautifulSoup(r.text, 'lxml')
+soup = BeautifulSoup(r.content, 'lxml')
+titles = soup.select(".thumb")
+for title in titles:
+    print(title.text)
 
-print(soup)
