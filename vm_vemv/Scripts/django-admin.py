2e1a10eb4ff5663e6066637b80eba1e87083e00b
takomisonikomi@gmail.com <takomisonikomi@gmail.com>
2020-03-24 12:00:01 +0900

    venv再削除

--- vm_vemv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\t-morieda\documents\workspace\python\vm\vm_vemv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
