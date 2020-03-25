e2ee8db4f73060c93aaf1ea8253ab18f073529e8
takomisonikomi@gmail.com <takomisonikomi@gmail.com>
2020-03-25 10:18:23 +0900

    イメージを除いてデータ取得完了

--- vm/scraping/getInfoKirin.py
+++ vm/scraping/getInfoKirin.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import sqlite3,sys,os,json,re,datetime,time
+import sqlite3
 
-con = sqlite3.connect("../db/vm.sqlite3")
+con = sqlite3.connect("/vm.sqlite3")
 cur = con.cursor()
 
 url = 'https://www.kirin.co.jp/products/list/nutrition/softdrink/'
@@ -10,42 +10,42 @@ r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml',from_encoding='utf-8')
 titles = soup.select(".thumb")
 materials = soup.select('.explanation')
-
+allrs = soup.select('.nutrition tr')
+allrs = soup.select('.nutrition td:nth-of-type(3)')
 
 tr = soup.select(".nutrition  .thumb")
 tr = len(tr)
 
-def getInfoKirin(titles,materials):
-    for i in range(tr):
-        n=1
-        for title in titles:
-            title = title.text
-            title = title.replace('\u3000', '')
 
-        for material in materials:
-            material = material.text
-            material = material.replace('\u3000', '')
+def getInfoKirin(titles,materials,allrs):
+    for i in range(tr):
+        title = titles[i]
+        title = title.text.replace('\u3000', ' ')
+
+        material = materials[i]
+        material = material.text
+
+        allr = allrs[i]
+        allr = allr.text
+
 
     # titles = soup.select(".thumb")
     # for title in titles:
     #     title = title.text
     #     title = title.replace('\u3000', '')
 
-        sql='REPLACE INTO drink (id,title,material,allr,image,brand) VALUES (?,?,?,?,?,?);'
+        sql='REPLACE INTO drink (id,title,material,allr,image) VALUES (?,?,?,?,?,?);'
 
-        data = (n,title,material,'','','')
+        data = (i,title,material,allr,'',)
         print(data)
 
         cur.execute(sql,data)
-
-        n += 1
-
 
         i += 1
     con.commit()
     con.close()
 
-getInfoKirin(titles,materials)
+getInfoKirin(titles,materials,allrs)
 
 
 
